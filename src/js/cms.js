4bbb073ccaf64e4ade602703630c6ebaabf15ec1
dindinet <2939526+dindinet@users.noreply.github.com>
2021-02-18 10:56:38 +0000

    [skip ci]

--- src/js/cms.js
+++ src/js/cms.js
@@ -1,5 +1,6 @@
 import React from "react";
 import CMS from "netlify-cms-app";
+import cloudinary from 'netlify-cms-media-library-cloudinary';
 
 // Import main site styles as a string to inject into the CMS preview pane
 import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css";
